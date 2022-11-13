c16673384dda60e5ac11f58f52274be5b8376f3b
Ping Chen <remotenonsense@gmail.com>
2022-11-13 15:25:29 +0900

    chore(web): improve web page layout and styles

--- app/client/src/App.vue
+++ app/client/src/App.vue
@@ -1,26 +1,38 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <router-link to="/">Sentio Composer</router-link>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
+body {
+  background: #f8fafc;
+}
+
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  width: 70rem;
+  max-width: 100vw;
+  margin: 0 auto;
 }
 
 nav {
-  padding: 30px;
+  padding: 1rem 0;
+  font-size: 1.25rem;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  text-decoration: none;
 }
 
 nav a.router-link-exact-active {
