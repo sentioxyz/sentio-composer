87434ba193e54a0c0f14821e3ef1214f76e5f094
Poytr1 <pooytr1@gmail.com>
2022-12-10 18:03:28 +0800

    fix the test

--- src/module_resolver.rs
+++ src/module_resolver.rs
@@ -3,7 +3,9 @@ use anyhow::{anyhow, Result};
 use aptos_sdk::rest_client::aptos_api_types::MoveModule;
 use aptos_sdk::rest_client::{Client, MoveModuleBytecode};
 use log::{debug, warn};
+use move_core_types::account_address::AccountAddress;
 use move_core_types::language_storage::ModuleId;
+use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::path::Path;
 use tokio::runtime::Runtime;
@@ -58,22 +60,25 @@ impl CacheModuleResolver {
                     if let Ok(mod_) =
                         MoveModuleBytecode::new(module.bytecode.0.to_vec()).try_parse_abi()
                     {
-                        abi = mod_.abi;
+                        abi = mod_.abi.clone();
+                        let module_address = abi.as_ref().unwrap().address.to_string();
+                        let module_name = abi.as_ref().unwrap().name.as_str();
                         // caching the module into memory before head
                         // self.module_cache.insert(
                         //     module_id.clone(),
                         //     (Some(module.bytecode.0.clone()), abi.clone()),
                         // );
-                        self.cache_module_to_disk(module_id, module.bytecode.0.clone());
-                        return abi.as_ref().unwrap().name.as_str() == module_id.name().as_str();
+                        self.cache_module_to_disk(
+                            module_address.as_str(),
+                            module_name,
+                            module.bytecode.0.clone(),
+                        );
+                        return module_name == module_id.name().as_str();
                     }
                     false
                 });
             if let Some(module) = matched_module {
                 debug!("load module: {}::{}", addr, module_id.name().as_str());
-                // if addr.to_hex_literal() == "0x1" || addr.to_hex_literal() == "0x3" {
-                //     self.cache_module_to_disk(module_id, module.bytecode.0.clone());
-                // }
                 return Ok((Option::from(module.bytecode.0.clone()), abi));
             }
             Ok((None, abi))
@@ -84,15 +89,16 @@ impl CacheModuleResolver {
         &self,
         module_id: &ModuleId,
     ) -> Option<(Option<Vec<u8>>, Option<MoveModule>)> {
-        let module_cache_key = self.get_cache_key(module_id);
+        let module_address = module_id.address().to_hex_literal();
+        let module_name = module_id.name().as_str();
+        let module_cache_key = self.get_cache_key(module_address.as_str(), module_name);
         let cache_path = self.get_cache_path(self.cache_folder.clone());
         let cached_module = cacache::read_sync(cache_path, module_cache_key);
         match cached_module {
             Ok(m) => {
                 debug!(
                     "loaded module from cache: {}::{}",
-                    module_id.address(),
-                    module_id.name().as_str()
+                    module_address, module_name
                 );
                 let _abi = MoveModuleBytecode::new(m.clone())
                     .try_parse_abi()
@@ -107,18 +113,18 @@ impl CacheModuleResolver {
         None
     }
 
-    fn cache_module_to_disk(&self, module_id: &ModuleId, bytecode: Vec<u8>) {
+    fn cache_module_to_disk(&self, module_address: &str, module_name: &str, bytecode: Vec<u8>) {
+        debug!("Caching {}::{} to disk", module_address, module_name);
         cacache::write_sync(
             self.get_cache_path(self.cache_folder.clone()),
-            self.get_cache_key(module_id),
+            self.get_cache_key(module_address, module_name),
             bytecode,
         )
         .expect("Failed to cache the module");
     }
 
-    fn get_cache_key(&self, module_id: &ModuleId) -> String {
-        let mut module_cache_key = module_id.address().to_string().to_owned();
-        let module_name = module_id.name().as_str();
+    fn get_cache_key(&self, module_address: &str, module_name: &str) -> String {
+        let mut module_cache_key = module_address.to_string();
         module_cache_key.push_str(module_name);
         module_cache_key.push_str(self.network.as_str());
         module_cache_key
